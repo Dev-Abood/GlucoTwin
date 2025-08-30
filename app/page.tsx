4a62118796ab1eb3d59f8b588a5a0378b3e84610
Dev-Abood <abdullasayed10@gmail.com>
2025-08-30 15:17:50 +0400

    Final fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ TODO:
 Create the layout of the landing page, link other pages using router
 */
 
-// test
-// test 2
-
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
